bd472c3ea875f963120efd38487d6788e95b101a
Mike Cheng <michael.cheng@kik.com>
2016-02-27 22:12:12 -0500

    Import playsource service proto

--- codegen.sh
+++ codegen.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-# Generate the CPP files
+# Generate crowdsound cpp files
 protoc --grpc_out=src/ --plugin=protoc-gen-grpc=$(which grpc_cpp_plugin) proto/crowdsound_service.proto
 protoc --cpp_out=src/ proto/crowdsound_service.proto
+
+# Generate playsouece cpp files
+protoc --grpc_out=src/ --plugin=protoc-gen-grpc=$(which grpc_cpp_plugin) proto/playsource_service.proto
+protoc --cpp_out=src/ proto/playsource_service.proto
