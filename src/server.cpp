f2d1739fef2889e96de01928dba762ab67579356
Mike Cheng <michael.cheng@kik.com>
2016-03-14 19:41:15 -0400

    Proper vote logic

--- src/server.cpp
+++ src/server.cpp
@@ -294,8 +294,9 @@ Status CrowdSoundImpl::VoteSkip(ServerContext* context, const VoteSkipRequest* r
         return Status(StatusCode::INTERNAL, status.message());
     }
 
-    float percentage = (float)(skip_voters_.size()) / 2.0f;
+    float percentage = (float)skip_voters_.size() / (float)users;
 
+    cout << "[algorithm]" << "vote ratio: " << percentage << " [" << skip_voters_.size() << ", " << users << "]" << endl;
     if (percentage > 0.5f) {
         playsource_->skipSong();
         skip_voters_.clear();
