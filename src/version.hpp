fc897f0d0f263843d66172cd80f91e6d1189de62
Mike Cheng <michael.cheng@kik.com>
2016-03-17 14:03:58 -0400

    Version

--- src/version.hpp
+++ src/version.hpp
@@ -4,8 +4,8 @@
 
 #include <string>
 
-const std::string CROWDSOUND_VERSION = "ee466e8";
-const std::string PLAYSOURCE_VERSION = "a3c9473";
+const std::string CROWDSOUND_VERSION = "ef0daa9";
+const std::string PLAYSOURCE_VERSION = "57c94c6";
 const std::string ALGORITHM_VERSION  = "faf464c";
 const std::string SKRILLEX_VERSION   = "48bfe14";
 const std::string GRPC_VERSION       = "0.11-1";
